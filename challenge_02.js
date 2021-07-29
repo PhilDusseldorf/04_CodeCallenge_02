65e2093f3e45c4163466daf009d747b15e60b6f4
Phil <p.pick@kladusch.de>
2021-07-29 16:20:06 +0200

    final

--- challenge_02.js
+++ challenge_02.js
@@ -15,3 +15,33 @@ Alter 13-17 // Cola
 --> "Max trinkt Cola."
 
 /* Getränke-Challenge */
+
+// set variables
+let drinkerAge = 101;
+
+// get a name
+let drinkerName = prompt("Person: ");
+
+// get the right age
+// ask until correct age is given
+while (drinkerAge < 0 || drinkerAge > 100)
+{
+    drinkerAge = prompt("Person's age (0-100 years): ");
+}
+
+//SWITCH
+switch (true) {
+    case (drinkerAge >= 0 && drinkerAge <= 5):
+        console.log(drinkerName + " drinks milk.");
+        break;
+    case (drinkerAge >= 6 && drinkerAge <= 12):
+        console.log(drinkerName + " drinks juice.");
+        break;
+    case (drinkerAge >= 13 && drinkerAge <= 17):
+        console.log(drinkerName + " drinks juice.");
+        break;
+    default:
+        console.log(drinkerName + " drinks wine.");
+        break;
+}
+ 
